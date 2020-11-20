6fd173914f01c279ba75eef494fc3e105ace84a5
Jure Erznožnik <jure@velis.si>
2020-11-20 02:57:01 +0100

    Initial commit

---
+++ django_project_settings/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'django project settings'
+__version__ = '0.0.1'
+__author__ = 'Jure Erznožnik'
+__email__ = 'jure.erznoznik@gmail.com'
+__license__ = 'BSD 3-Clause'
+__copyright__ = 'Copyright 2020-present Jure Erznožnik'
+
+VERSION = __version__
---
+++ django_project_settings/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import swapper
+
+# Create your models here.
+
+
+class BaseProject(models.Model):
+    name = models.CharField(max_length=80, null=False, blank=False, db_index=True)
+    slug = models.SlugField(max_length=80, null=False, blank=False, db_index=True)
+    description = models.TextField(null=True, blank=True)
+    logo = models.FileField()
+
+    class Meta:
+        abstract = True
+
+
+class Project(BaseProject):
+    class Meta:
+        swappable = swapper.swappable_setting('django_project_settings', 'Project')
+
+
+class BaseProfile(User):
+    """
+    User profile. We start with some easy common settings
+    """
+    bio = models.TextField(max_length=500, null=True, blank=True)
+    phone_number = models.CharField(max_length=20,null=True, blank=True)
+    language = models.CharField(max_length=10, null=True, blank=True)  # This one will list all supported languages
+    theme = models.CharField(max_length=10, null=True, blank=True)  # This one will list all supported themes
+    avatar = models.FileField()
+
+    class Meta:
+        abstract = True
+
+
+class Profile(BaseProfile):
+    class Meta:
+        swappable = swapper.swappable_setting('django_project_settings', 'Profile')
+
+
+# noinspection PyUnusedLocal
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        swapper.load_model('django_project_settings', 'Profile').objects.create(user=instance)
+
+
+# noinspection PyUnusedLocal
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
