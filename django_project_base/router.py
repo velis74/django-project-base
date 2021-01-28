f3c086c58a00ae3782dac6e39d381d89b9bfcb23
KlemenŠpruk <spruk.klemen@gmail.com>
2021-01-28 11:43:09 +0100

    filtered out rest_registration profile api

--- django_project_base/router.py
+++ django_project_base/router.py
@@ -7,6 +7,15 @@ from django_project_base.base.rest.project_base_router import ProjectBaseRouter
 from django_project_base.rest.profile import ProfileViewSet
 from django_project_base.rest.project import ProjectViewSet
 
+def filter_rest_documentation_endpoints(endpoints: list) -> list:
+    _endpoints: list = []
+    for (path, path_regex, method, callback) in endpoints:
+        module: str = getattr(getattr(callback, 'view_class', object()), '__module__', '')
+        exclude: bool = 'profile' in path and 'rest_registration' in module
+        if not exclude:
+            _endpoints.append((path, path_regex, method, callback))
+    return _endpoints
+
 
 class RestRouter(ProjectBaseRouter):
     pass
@@ -16,13 +25,17 @@ django_project_base_router = RestRouter(trailing_slash=False)
 django_project_base_router.register(r'project', ProjectViewSet, basename='project-base-project')
 django_project_base_router.register(r'profile', ProfileViewSet, basename='profile-base-project')
 
+
 SPECTACULAR_DEFAULTS['TITLE'] = 'Rest documentation'
 SPECTACULAR_DEFAULTS['DESCRIPTION'] = 'Api documentation'
 SPECTACULAR_DEFAULTS['VERSION'] = '0.0.1'
+SPECTACULAR_DEFAULTS['PREPROCESSING_HOOKS'] = [
+    'django_project_base.router.filter_rest_documentation_endpoints'
+]
 
 django_project_base_urlpatterns = [
-    path('dpb-rest-account/', include('rest_registration.api.urls')),
-    url(r'dpb-rest/', include(django_project_base_router.urls)),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', include('rest_registration.api.urls')),
+    url(r'', include(django_project_base_router.urls)),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema',), name='swagger-ui'),
 ]
