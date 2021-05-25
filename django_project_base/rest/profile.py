692e182a67e8b2526f28ed676c872500c956fbb4
Uros Tement <ut@scantron.si>
2021-05-25 09:59:59 +0200

    Load profile with swapper

--- django_project_base/rest/profile.py
+++ django_project_base/rest/profile.py
@@ -1,3 +1,4 @@
+import swapper
 from django.db.models import CharField, Model, Q
 from django.db.models.functions import Cast
 from django_project_base.base.rest.serializer import Serializer as ProjectBaseSerializer
@@ -17,10 +18,10 @@ class ProfileSerializer(ProjectBaseSerializer):
 
 class ProfileViewSet(ProjectBaseViewSet):
     def get_queryset(self):
-        return self._get_model('DJANGO_PROJECT_BASE_PROFILE_MODEL').objects.all()
+        return swapper.load_model('django_project_base', 'Profile').objects.all()
 
     def get_serializer_class(self):
-        ProfileSerializer.Meta.model = self._get_model('DJANGO_PROJECT_BASE_PROFILE_MODEL')
+        ProfileSerializer.Meta.model = swapper.load_model('django_project_base', 'Profile')
         return ProfileSerializer
 
     @action(methods=['GET'], detail=False, url_path='current', url_name='profile-current')
@@ -28,10 +29,11 @@ class ProfileViewSet(ProjectBaseViewSet):
         user: Model = getattr(request, 'user', None)
         if not user:
             raise exceptions.AuthenticationFailed
-        serializer = self.get_serializer(user.userprofile)
+        serializer = self.get_serializer(
+            getattr(user, swapper.load_model('django_project_base', 'Profile')._meta.model_name))
         response_data: dict = serializer.data
         if getattr(request, 'GET', None) and request.GET.get('decorate', '') == 'default-project':
-            project_model: Model = self._get_model('DJANGO_PROJECT_BASE_PROJECT_MODEL')
+            project_model: Model = swapper.load_model('django_project_base', 'Project')
             response_data['default-project'] = None
             if project_model:
                 ProjectSerializer.Meta.model = project_model
@@ -44,7 +46,7 @@ class ProfileViewSet(ProjectBaseViewSet):
         user: Model = getattr(request, 'user', None)
         if not user:
             raise exceptions.AuthenticationFailed
-        profile_model: Model = self._get_model('DJANGO_PROJECT_BASE_PROFILE_MODEL')
+        profile_model: Model = swapper.load_model('django_project_base', 'Profile')
         fields: list = [f for f in profile_model._meta.fields if not f.is_relation]
         annotations: dict = {"as_str_%s" % v.name: Cast(v.name, CharField()) for v in fields}
         queries: list = [Q(**{"as_str_%s__icontains" % f.name: query}) for f in fields]
