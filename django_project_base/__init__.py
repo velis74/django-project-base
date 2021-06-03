a1675f6b54dfa259260602926067adeda3e03733
KlemenŠpruk <spruk.klemen@gmail.com>
2021-06-03 12:27:08 +0200

    queryset fix

--- django_project_base/__init__.py
+++ django_project_base/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'django project base'
-__version__ = '0.1.6.2'
+__version__ = '0.1.6.3'
 __author__ = 'Jure Erznožnik'
 __email__ = 'jure.erznoznik@gmail.com'
 __license__ = 'BSD 3-Clause'
