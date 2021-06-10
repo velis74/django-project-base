3b43faa4cdae2a02074915d2363ee0e1aa619395
KlemenŠpruk <spruk.klemen@gmail.com>
2021-06-10 13:25:02 +0200

    new build

--- django_project_base/__init__.py
+++ django_project_base/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'django project base'
-__version__ = '0.1.6.4'
+__version__ = '0.1.6.5'
 __author__ = 'Jure Erznožnik'
 __email__ = 'jure.erznoznik@gmail.com'
 __license__ = 'BSD 3-Clause'
