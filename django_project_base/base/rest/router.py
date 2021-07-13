10c6e46aefc1fdfa2735ba69097c2e5a588e2a93
Uroš Tement <ut@scantron.si>
2021-07-13 13:03:22 +0200

    #127 združiti project_base_serializer.py in project_base_viewset.py (#28)
    Odstranjen /base/rest

--- django_project_base/base/rest/router.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-
-
-class Router(routers.DefaultRouter):
-    def extend(self, router):
-        self.registry.extend(router.registry)
--- django_project_base/notifications/rest/router.py
+++ django_project_base/notifications/rest/router.py
@@ -1,12 +1,12 @@
-from django_project_base.base.rest.router import Router
 from django_project_base.notifications.rest.maintenance_notification import UsersMaintenanceNotificationViewset
+from rest_framework.routers import DefaultRouter
 
 
-class NotificationsRouter(Router):
+class NotificationsRouter(DefaultRouter):
     pass
 
 
-notifications_router: NotificationsRouter = NotificationsRouter()
+notifications_router: NotificationsRouter = DefaultRouter()
 
 notifications_router.register(r'maintenance-notification', UsersMaintenanceNotificationViewset,
                               basename='maintenance-notification')
