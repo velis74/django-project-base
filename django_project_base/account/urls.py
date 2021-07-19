13a4abbe04266a732a465cf413bb556f401eb632
Uroš Tement <ut@scantron.si>
2021-07-19 11:55:53 +0200

    330 accounts podmodul: URLji in RESTi niso v podmodulu (#34)

--- django_project_base/account/urls.py
+++ django_project_base/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 
-from .router import accounts_router
+from .router import accounts_router, profile_router
 
 urlpatterns = [
     path('', include(accounts_router.urls)),
+    path('', include(profile_router.urls)),
 ]
