755cac01f24c5df1b92e03ca6ab2dd7adeae0d03
Uroš Tement <ut@scantron.si>
2021-07-23 14:10:59 +0200

    Label for account app and dark mode (#36)

--- django_project_base/account/apps.py
+++ django_project_base/account/apps.py
@@ -7,6 +7,7 @@ from django_project_base.settings_parser import parse_settings
 class DjangoProjectBaseAccountsConfig(AppConfig):
     name = ACCOUNT_APP_ID
     verbose_name = "Django Project Base Account management"
+    label = 'django_project_base.account'
 
     def ready(self):
         parse_settings(ACCOUNT_SETTINGS)
