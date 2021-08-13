3a116e9a52c78f4d5b607bf6f953c1d620ad7f79
KlemenŠpruk <spruk.klemen@gmail.com>
2021-08-13 13:59:33 +0200

    removed redundant api call

--- django_project_base/js_app/src/session.js
+++ django_project_base/js_app/src/session.js
@@ -8,16 +8,18 @@ class Session {
     Store.set('redirect-to-auth', false);
     ApiClient.post('/account/login/',
       { login: username, password }).then(() => {
-      ApiClient.get('/account/profile/current').then((response) => {
-        Store.set('current-user', response.data);
-        //  WE DO NOT HAVE CURRENT PROJECT FOR USER IMPLEMENTED, FOR NOW WE SKIP THIS
-        // eslint-disable-next-line max-len
-        // Store.set('current-project', response.data['default-project'][PROJECT_TABLE_PRIMARY_KEY_PROPERTY_NAME]);
-        document.dispatchEvent(createEvent('login', response.data));
-        /* redirect to root */
+      // ApiClient.get('/account/profile/current').then((response) => {
+      //   Store.set('current-user', response.data);
+      //  todo: WE DO NOT HAVE CURRENT PROJECT FOR USER IMPLEMENTED, FOR NOW WE SKIP THIS
+      // eslint-disable-next-line max-len
+      // Store.set('current-project', response.data['default-project'][PROJECT_TABLE_PRIMARY_KEY_PROPERTY_NAME]);
+      // document.dispatchEvent(createEvent('login', response.data));
+      /* redirect to root */
+      // window.location.href = '/';
+      // });
+      Session.checkLogin().then(() => {
         window.location.href = '/';
       });
-      Session.checkLogin();
     }).catch(() => {
       Store.delete('current-user');
     });
