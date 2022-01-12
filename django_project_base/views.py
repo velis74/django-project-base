0505df18fb8d3aeb0a0c91d73e7ba7a00d7c739a
KlemenŠpruk <spruk.klemen@gmail.com>
2022-01-12 14:27:34 +0100

    tox fix

--- django_project_base/views.py
+++ django_project_base/views.py
@@ -8,6 +8,7 @@ def documentation_view(request: object, path='', document_root=None) -> FileResp
         path = 'index.html'
     return static.serve(request=request, path=path, document_root=document_root)
 
+
 def browser_update_script(request: object) -> HttpResponse:
     return HttpResponse(requests.get('https://browser-update.org/update.min.js', verify=False).content.decode(),
                         content_type='application/javascript')
