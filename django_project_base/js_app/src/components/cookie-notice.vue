7bffef61352aab7843f97f45de3662da3ccc11b5
KlemenŠpruk <spruk.klemen@gmail.com>
2022-01-14 16:07:30 +0100

    added cokkie notice component

---
+++ django_project_base/js_app/src/components/cookie-notice.vue
@@ -0,0 +1,33 @@
+<template>
+  <cookie-law :message="message || defaultMessage"/>
+</template>
+<script>
+import translationsMixin from 'dynamicforms/src/mixins/translationsMixin';
+import CookieLaw from 'vue-cookie-law';
+
+export default {
+  name: 'CookieNotice',
+  components: {
+    CookieLaw,
+  },
+  mixins: [translationsMixin],
+  props: {
+    message: {
+      type: String,
+      // eslint-disable-next-line max-len
+      default: null,
+    },
+  },
+  computed: {
+    defaultMessage() {
+      // eslint-disable-next-line max-len
+      return this.gettext('Our site saves small pieces of text information (cookies) on your device in order to deliver better content and for statistical purposes. You can disable the usage of cookies by changing the settings of your browser. By browsing our website without changing the browser settings you grant us permission to store that information on your device.');
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
