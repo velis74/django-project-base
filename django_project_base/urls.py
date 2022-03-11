40b38af91ae3e255149fb9592a7236d7004fd8f4
KlemenŠpruk <spruk.klemen@gmail.com>
2022-03-11 14:39:57 +0100

    parse permissions classes

--- django_project_base/urls.py
+++ django_project_base/urls.py
@@ -12,6 +12,10 @@ SPECTACULAR_DEFAULTS['PREPROCESSING_HOOKS'] = [
     'django_project_base.router.filter_rest_documentation_endpoints'
 ]
 
+SPECTACULAR_DEFAULTS['POSTPROCESSING_HOOKS'] = [
+    'django_project_base.router.postprocess_rest_documentation'
+]
+
 urlpatterns = [
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     re_path('browser-update/js-script', browser_update_script, name='browser-update-script'),
