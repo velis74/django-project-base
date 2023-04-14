8263bfc0c2c70948847803eebecf814dd24a62a4
Jure Erznožnik <jure@velis.si>
2023-04-14 13:08:00 +0200

    fix the logout trailing slash issue

--- django_project_base/account/router.py
+++ django_project_base/account/router.py
@@ -16,7 +16,6 @@ from django_project_base.account.rest.login import LoginViewset
 accounts_router = DFRouter(trailing_slash=True)
 
 accounts_router.register(r"", SocialAuthProvidersViewSet, basename="account")
-accounts_router.register(r"", LogoutViewSet, basename="account")
 accounts_router.register(r"change-password", ChangePasswordViewSet, basename="change-password")
 accounts_router.register(r"", ResetPasswordViewSet, basename="account")
 accounts_router.register(r"", RegisterViewSet, basename="account")
@@ -27,3 +26,4 @@ profile_router = DFRouter(trailing_slash=False)
 profile_router.register(r"profile", ProfileViewSet, basename="profile-base-project")
 profile_router.register_single_record(r"impersonate", ImpersonateUserViewset, basename="profile-base-impersonate-user")
 profile_router.register_single_record(r"login", LoginViewset, basename="profile-base-login")
+profile_router.register(r"", LogoutViewSet, basename="account")
