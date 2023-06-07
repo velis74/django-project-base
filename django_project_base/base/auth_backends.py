471bf9c4a3af6bf23a0a9b3a81a2d3e90db9b529
Uroš Tement <ut@scantron.si>
2023-06-07 10:32:19 +0200

    #584 Fix user processing in middlewares (#64)
    * #584 Fix user processing in middlewares
    
    * Added try/catch
    
    * Fixed exception

--- django_project_base/base/auth_backends.py
+++ django_project_base/base/auth_backends.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.core.cache import cache
 from django.db.models.signals import post_delete, post_save
+import swapper
 
 from django_project_base.settings import USER_CACHE_KEY
 
@@ -19,7 +20,16 @@ def invalidate_cache(sender, instance, **kwargs):
     user_cache_invalidate(instance)
 
 
-class UsersCachingBackend(ModelBackend):
+class UsersBackend(ModelBackend):
+    def get_user(self, user_id):
+        try:
+            user = swapper.load_model("django_project_base", "Profile").objects.get(pk=user_id)
+        except Exception as e:
+            return None
+        return user if self.user_can_authenticate(user) else None
+
+
+class UsersCachingBackend(UsersBackend):
     def __init__(self) -> None:
         super().__init__()
         post_save.connect(invalidate_cache, sender=get_user_model())
