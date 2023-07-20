1b2fd7163d00b5896d0d4ea1abab6f37e31f6fff
adamPrestor <adam.prestor@gmail.com>
2023-07-20 13:23:01 +0200

    Refactor impersonate field into ajax field

--- django_project_base/base/fields.py
+++ django_project_base/base/fields.py
@@ -2,7 +2,7 @@ import swapper
 from django.core.validators import RegexValidator
 from django.db.models import fields
 from django.utils.translation import gettext_lazy as _
-from dynamicforms import fields as dffields
+from dynamicforms import fields as df_fields
 
 
 class HexColorField(fields.CharField):
@@ -18,10 +18,13 @@ class HexColorField(fields.CharField):
         )
 
 
-class UserRelatedField(dffields.PrimaryKeyRelatedField):
+class UserRelatedField(df_fields.PrimaryKeyRelatedField):
     def __init__(self, **kwargs):
+        kwargs["url_reverse"] = "profile-base-project-list"
         kwargs["query_field"] = "full_name"
         kwargs["additional_parameters"] = dict(select=1)
+        kwargs["text_field"] = "full_name"
+        kwargs["value_field"] = "id"
         super().__init__(**kwargs)
 
     def get_queryset(self):
