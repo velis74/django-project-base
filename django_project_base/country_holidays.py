451f32767e976cfff309272cf6b0258356692db2
Uros Tement <ut@scantron.si>
2023-07-27 14:46:41 +0200

    Added test function

--- django_project_base/country_holidays.py
+++ django_project_base/country_holidays.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 from requests import Timeout
 from rest_framework import status
 
-holidays_api_url: str = 'https://date.nager.at/Api/v2/PublicHolidays/%d/%s'
+holidays_api_url: str = "https://date.nager.at/Api/v2/PublicHolidays/%d/%s"
 
 
 class RetrieveHolidaysException(Exception):
@@ -15,10 +15,11 @@ class RetrieveHolidaysException(Exception):
 
 def get_holidays(country_alpha_2_code: str, year: int) -> List[dict]:
     try:
-        assert country_alpha_2_code and isinstance(country_alpha_2_code, str) and len(
-            country_alpha_2_code) == 2, _("Not valid country alpha 2 code")
+        assert country_alpha_2_code and isinstance(country_alpha_2_code, str) and len(country_alpha_2_code) == 2, _(
+            "Not valid country alpha 2 code"
+        )
         assert isinstance(year, int) and year > 1900, _("Not a valid year")
-        ck: str = f'country-holidays-{year}-{country_alpha_2_code}'
+        ck: str = f"country-holidays-{year}-{country_alpha_2_code}"
         cached_data: Optional[List[dict]] = cache.get(ck)
         if cached_data is not None:
             return cached_data
@@ -28,4 +29,8 @@ def get_holidays(country_alpha_2_code: str, year: int) -> List[dict]:
         cache.set(ck, holidays_data, timeout=None)
         return holidays_data
     except (AssertionError, Timeout) as e:
-        raise RetrieveHolidaysException(_('Error retrieving holidays: {}'.format(e))) from None
+        raise RetrieveHolidaysException(_("Error retrieving holidays: {}".format(e))) from None
+
+
+def test_function():
+    return False
