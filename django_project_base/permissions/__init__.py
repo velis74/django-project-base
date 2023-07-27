2742fcba646ee9ee5eba6ad432af79399efce07a
adamPrestor <adam.prestor@gmail.com>
2023-07-27 12:55:35 +0200

    Remove debug print

--- django_project_base/permissions/__init__.py
+++ django_project_base/permissions/__init__.py
@@ -5,7 +5,6 @@ from rest_framework.settings import api_settings
 class BasePermissions(DRFBasePermission):
     def has_permission(self, request, view):
         default_permission_classes = getattr(api_settings, "DEFAULT_PERMISSION_CLASSES", [])
-        print("ding ding")
 
         return all(
             [permission_class().has_permission(request, view) for permission_class in default_permission_classes]
