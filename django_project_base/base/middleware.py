58e030acd4056f492e6de111f9a6787fa2698b42
Jure Erznožnik <jure@velis.si>
2023-08-01 10:58:10 +0200

    0.3.11: non-project paths excluded

--- django_project_base/base/middleware.py
+++ django_project_base/base/middleware.py
@@ -21,8 +21,9 @@ def get_parameter(request, value_name: str, url_part: str) -> Optional[object]:
         return value_from_header
 
     path_parts = request.path_info.split("/")
-    if isinstance(url_part, int):
-        return path_parts[url_part] if len(path_parts) > url_part else None
+    if isinstance(url_part, tuple) and isinstance(url_part[0], int) and isinstance(url_part[1], list):
+        parm = path_parts[url_part[0]] if len(path_parts) > url_part[0] else None
+        return parm if parm not in url_part[1] else None
 
     try:
         project_info = next(iter(filter(lambda f: f and url_part in f, path_parts)))
