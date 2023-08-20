571b5e84dc4c352c86fc1c8abfca81390d3362c6
Jure Erznožnik <jure@velis.si>
2023-08-20 14:45:20 +0200

    #677 - (partial solution) id field was visible because of model inheritance

--- django_project_base/utils.py
+++ django_project_base/utils.py
@@ -1,4 +1,14 @@
 from enum import IntEnum
+from typing import Union
+
+from django.db.models import Model, QuerySet
+from rest_framework.utils.model_meta import get_field_info
+
+
+def get_pk_name(obj: Union[Model, QuerySet]):
+    if isinstance(obj, QuerySet):
+        return get_field_info(obj.model).pk.name
+    return get_field_info(obj).pk.name
 
 
 def set_django_security(django_settings, deploy=True, swagger_version=None):
