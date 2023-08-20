8e5aca1785c926c0abc021b97b2f557965121daa
Jure Erznožnik <jure@velis.si>
2023-08-20 11:26:59 +0200

    support current project retrieval

--- django_project_base/account/middleware.py
+++ django_project_base/account/middleware.py
@@ -1,11 +1,11 @@
 import json
 
 from django.conf import settings
-from django.contrib.sessions.middleware import SessionMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware as SessionMiddlewareBase
 from rest_framework.authentication import get_authorization_header
 
 
-class SessionMiddleware(SessionMiddleware):
+class SessionMiddleware(SessionMiddlewareBase):
     def get_request_params(self, request):
         if request.method in ["POST", "PUT", "PATCH", "DELETE"]:
             body_data = json.loads(request.body.decode("utf-8"))
@@ -24,9 +24,13 @@ class SessionMiddleware(SessionMiddleware):
             session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME)
 
         request.session = self.SessionStore(session_key)
-        curr_project_setting_name = settings.DJANGO_PROJECT_BASE_BASE_REQUEST_URL_VARIABLES["project"]["value_name"]
-        if curr_project_value := getattr(request, curr_project_setting_name, None):
-            request.session[curr_project_setting_name] = curr_project_value
+        current_project_attr = (
+            getattr(settings, "DJANGO_PROJECT_BASE_BASE_REQUEST_URL_VARIABLES", {})
+            .get("project", {})
+            .get("value_name", None)
+        )
+        if current_project_attr and (curr_project_value := getattr(request, current_project_attr, None)):
+            request.session[current_project_attr] = curr_project_value
 
     def process_response(self, request, response):
         if getattr(response, "returntype", None) == "json":
