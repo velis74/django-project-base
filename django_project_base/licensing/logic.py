e1330637700c59a78f43fe6906a95cbe8c6bdd38
KlemenSpruk <spruk.klemen@gmail.com>
2023-08-30 13:19:59 +0200

    #679 Ne razumem kako naj bi basic licensing package deloval (#106)
    * new version of dynamicforms
    
    * for reset passwd always return 200
    
    * saving
    
    * calculate usage from amount
    
    * fixed migration for license
    
    * tox fix
    
    * reverted change in package json
    
    * squashed migrations

--- django_project_base/licensing/logic.py
+++ django_project_base/licensing/logic.py
@@ -1,54 +1,51 @@
 from typing import List
 
-from django.apps import apps
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
-from django.db import connections, models
-from django.db.models.functions import Cast
+from django.db import connections
+from django.db.models import Model, Sum
 from django.utils.translation import gettext
 from rest_framework.exceptions import PermissionDenied
 
 from django_project_base.licensing.models import LicenseAccessUse
-from django_project_base.utils import get_pk_name
 
 MONTHLY_ACCESS_LIMIT_IN_CURRENCY_UNITS = 86.97  # TODO: read from package
 
 
 class LogAccessService:
-    def log(self, user_profile_pk, channels: List[str], object_pk: str, on_sucess=None, **kwargs):
+    def log(
+        self,
+        user_profile_pk,
+        notifications_channels_state: List[str],
+        record: Model,
+        item_price: float,
+        comment: str,
+        on_sucess=None,
+        **kwargs
+    ):
         if getattr(settings, "TESTING", False) and on_sucess:
             on_sucess()
             return
 
-        # only notifications supported for now
-        model_data = settings.LICENSE_ACCESS_USE_CONTENT_TYPE_MODEL.split(".")
         db_connection = "default"
         if kwargs.get("db") and kwargs["db"] != "default":
             db_connection = kwargs["db"]
             connections["default"] = connections[db_connection]
 
-        content_type = ContentType.objects.get_for_model(model=apps.get_model(model_data[0], model_data[1]))
+        content_type = ContentType.objects.get_for_model(model=record._meta.model)
 
-        content_type_model = content_type.model_class()
-
-        current_items = LicenseAccessUse.objects.filter(user_id=str(user_profile_pk), content_type=content_type)
-
-        _channels = list(
-            content_type_model.objects.annotate(
-                pkstr=Cast(get_pk_name(content_type_model), output_field=models.CharField())
-            )
-            .filter(pkstr__in=current_items.values_list("content_type_object_id", flat=True))
-            .values_list("sent_channels", flat=True)
+        used = (
+            LicenseAccessUse.objects.filter(user_id=str(user_profile_pk), content_type=content_type)
+            .aggregate(Sum("amount"))
+            .get("amount__sum", None) or 0
         )
+        if notifications_channels_state:
+            items = {i: notifications_channels_state.count(i) for i in notifications_channels_state}
+            from django_project_base.notifications.base.enums import ChannelIdentifier
 
-        ch_one = {i: _channels.count(i) for i in _channels}
-        ch_two = {i: channels.count(i) for i in channels}
-        from django_project_base.notifications.base.enums import ChannelIdentifier
-
-        chl_prices = {i.name: i.notification_price for i in ChannelIdentifier.supported_channels()}
-        used = 0
-        for used_channel in list(set(list(ch_one.keys()) + list(ch_two.keys()))):
-            used += chl_prices.get(used_channel, 0) * (ch_one.get(used_channel, 0) + ch_two.get(used_channel, 0))
+            chl_prices = {i.name: i.notification_price for i in ChannelIdentifier.supported_channels()}
+            for used_channel in list(set(list(items.keys()))):
+                used += chl_prices.get(used_channel, 0) * items.get(used_channel, 0)
 
         allowed_users = getattr(settings, "NOTIFICATIONS_ALLOWED_USERS", [])
         if not allowed_users:
@@ -66,12 +63,11 @@ class LogAccessService:
             if on_success:
                 accesses_used = on_success
 
-        [
-            LicenseAccessUse.objects.using(db_connection).create(
-                type=LicenseAccessUse.USE,
-                user_id=str(user_profile_pk),
-                content_type_object_id=str(object_pk).replace("-", ""),
-                content_type=content_type,
-            )
-            for au in range(0, accesses_used)
-        ]
+        LicenseAccessUse.objects.using(db_connection).create(
+            type=LicenseAccessUse.UseType.USE,
+            user_id=str(user_profile_pk),
+            content_type_object_id=str(record.pk).replace("-", ""),
+            content_type=content_type,
+            amount=accesses_used * item_price,
+            comment=dict(comment=comment, count=accesses_used, item_price=item_price),
+        )
