1c0eb967f30a2f7f958033f365cbe6de826d14a3
Jure Erznožnik <jure@velis.si>
2023-08-30 17:27:22 +0200

    formatting

--- django_project_base/licensing/rest/rest.py
+++ django_project_base/licensing/rest/rest.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import status, viewsets
 from rest_framework.exceptions import APIException
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
