05a0d79faee490a78b2a6d529e86f09a55c064b7
adamPrestor <adam.prestor@gmail.com>
2023-08-31 16:14:16 +0200

    Reorder imports

--- django_project_base/account/rest/project_profiles.py
+++ django_project_base/account/rest/project_profiles.py
@@ -15,8 +15,8 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.utils import model_meta
 
-from django_project_base.account.rest.profile import ProfileViewSet, ProfileSerializer
-from .project_profiles_utils import get_project_members, filter_project_members_fields
+from django_project_base.account.rest.profile import ProfileSerializer, ProfileViewSet
+from .project_profiles_utils import filter_project_members_fields, get_project_members
 
 
 class ProjectProfilesSerializer(ProfileSerializer):
