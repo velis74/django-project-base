38cf2822901688aaa583655e4e3fed5063d27b58
KlemenSpruk <spruk.klemen@gmail.com>
2023-09-07 12:03:18 +0200

    resend notification (#118)

---
+++ django_project_base/notifications/management/commands/resend_notification.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.shortcuts import get_object_or_404
+
+from django_project_base.notifications.base.notification import Notification
+from django_project_base.notifications.models import DjangoProjectBaseNotification
+
+
+class Command(BaseCommand):
+    help = 'Resends notification. Example:  python manage.py resend_notification "178a46c2-2aa3-4a33-bad6-9af2d76f6891" 2'  # noqa: E501
+
+    def add_arguments(self, parser) -> None:
+        parser.add_argument('notification', type=str, help='Notification identifier (uuid string).')
+        parser.add_argument('user', type=str, help='User identifier (user sending notification).')
+
+    def handle(self, *args, **options):
+        Notification.resend(get_object_or_404(DjangoProjectBaseNotification, pk=str(options['notification'])),
+                            str(options["user"]))
+        return "ok"
