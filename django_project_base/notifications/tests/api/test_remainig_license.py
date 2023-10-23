5e24d362ceb6c31f0d0c4991e40dbfb38e49ebc3
KlemenSpruk <spruk.klemen@gmail.com>
2023-10-23 07:52:40 +0200

    allow negative access use as credit (#160)

--- django_project_base/notifications/tests/api/test_remainig_license.py
+++ django_project_base/notifications/tests/api/test_remainig_license.py
@@ -1,6 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
 
-from django_project_base.licensing.logic import LogAccessService, MONTHLY_ACCESS_LIMIT_IN_CURRENCY_UNITS
+from django_project_base.licensing.logic import LogAccessService
 from django_project_base.licensing.models import LicenseAccessUse
 from django_project_base.notifications.tests.notifications_transaction_test_case import NotificationsTransactionTestCase
 
@@ -14,8 +14,18 @@ class TestRemainingLicense(NotificationsTransactionTestCase):
             user_id=str(self.test_user.pk),
             content_type_object_id=str(mail.pk),
             content_type=ContentType.objects.get_for_model(mail._meta.model),
+            amount=-100,
+            comment=dict(comment="", count=10, item_price=1, sender=""),
+        )
+        LicenseAccessUse.objects.create(
+            type=LicenseAccessUse.UseType.USE,
+            user_id=str(self.test_user.pk),
+            content_type_object_id=str(mail.pk),
+            content_type=ContentType.objects.get_for_model(mail._meta.model),
             amount=used_amount,
             comment=dict(comment="", count=10, item_price=1, sender=""),
         )
-        usage = LogAccessService().report(user=self.test_user)
-        self.assertEqual(MONTHLY_ACCESS_LIMIT_IN_CURRENCY_UNITS - used_amount, usage["remaining_credit"])
+        log_service = LogAccessService()
+        usage = log_service.report(user=self.test_user)
+        credit = log_service._user_access_user_inflow(self.test_user.pk)
+        self.assertEqual(credit - used_amount, usage["remaining_credit"])
