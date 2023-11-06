bd730726bc1c9bbe3ba42de79ec5729e46d9b7aa
KlemenSpruk <spruk.klemen@gmail.com>
2023-11-06 08:19:02 +0100

    constant name fix

--- django_project_base/constants.py
+++ django_project_base/constants.py
@@ -11,4 +11,4 @@ INVITE_NOTIFICATION_TEXT = "invite-notification-link-text"
 
 NOTIFY_NEW_USER_SETTING_NAME = "notify-new-user-via-email-account-created"
 
-USE_EMAIL_IF_RECIPIENT_HAS_NO_PHONE_NUBER = "notify-user-via-email-if-no-phone-number"
+USE_EMAIL_IF_RECIPIENT_HAS_NO_PHONE_NUMBER = "notify-user-via-email-if-no-phone-number"
