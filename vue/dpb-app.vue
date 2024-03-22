aa3d56c14e289cb706f2ca4d87a9c36b84d8f4ed
adamPrestor <adam.prestor@gmail.com>
2024-03-22 17:47:15 +0100

    Create dpb app

---
+++ vue/dpb-app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { DfApp } from '@velis/dynamicforms';
+
+import LoginDialog from './components/user-session/login-dialog.vue';
+</script>
+
+<template>
+  <df-app>
+    <LoginDialog/>
+    <slot/>
+  </df-app>
+</template>
