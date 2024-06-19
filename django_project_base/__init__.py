61bbfd321aa4880718927bfd354775e2b18eb031
Klemen Pukl <brontes>
2024-06-19 10:52:27 +0200

    #86bz85z0d - NEW DF - Failing tests in github actions

--- django_project_base/__init__.py
+++ django_project_base/__init__.py
@@ -1,4 +1,5 @@
 __title__ = "django project base"
+# __version__ = "{version-string}"  #TODO: Switch back to version string when merging to main
 __version__ = "0.1.37.1"
 __author__ = "Jure Erznožnik"
 __email__ = "jure.erznoznik@gmail.com"
