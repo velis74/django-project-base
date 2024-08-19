1dc95057d1f798ea72457a597ea71d036868081a
Jure Erznožnik <jure@velis.si>
2024-08-19 16:13:21 +0200

    fix styles import

--- vue/main.ts
+++ vue/main.ts
@@ -4,6 +4,8 @@ import { createProjectBase } from './apps';
 import Vuetify from './plugins/vuetify';
 import TitlebarAppStandalone from './titlebar-app-standalone.vue';
 
+import 'vuetify/styles/main.css';
+import '@velis/dynamicforms/styles.css';
 import './assets/global.css';
 
 const app = createApp(TitlebarAppStandalone);
